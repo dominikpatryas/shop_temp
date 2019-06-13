6cc8d5179e7f71029306eaa8ce5e9196fc3b669f
Dominik Patryas <dominik.patryas@gmail.com>
2019-06-13 15:23:31 +0200

    init

---
+++ includes/header.php
@@ -0,0 +1,52 @@
+<?php
+
+require 'config/config.php';
+
+
+
+
+?>
+
+
+<html>
+<head>
+<!-- JS -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="assets/js/bootstrap.js"> </script>
+<script src="assets/js/bootbox.min.js"> </script>
+<script src="assets/js/jcrop_bits.js"> </script>
+<script src="assets/js/jquery.Jcrop.js"> </script>
+
+<!-- CSS -->
+
+<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.css">
+<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+
+</head>
+
+<body>
+
+<header>
+    <div class="log_nav">
+        <ul>
+            <li>Logowanie</li>
+            <li>Rejestracja</li>
+            <li>Kontakt</li>
+        </ul>
+    </div>
+    <div id="logo_img">
+        <img src="/assets/images/logo-amazon.jpg" alt="">
+    </div>
+    <nav>
+      <ul>
+        <li><a href="#">Telefony</a></li>
+        <li><a href="#">Telewizory</a></li>
+        <li><a href="#">AGD</a></li>
+        <li><a href="#">Dom</a></li>
+        <li><a href="#">Ogród</a></li>
+        <li><a href="#">Hobby</a></li>
+      </ul>
+    </nav>
+  </header>
+   
---
+++ index.php
@@ -0,0 +1,11 @@
+<?php 
+include("includes/header.php");
+// include("includes/classes/User.php");
+// include("includes/classes/Post.php");
+
+ ?>
+
+
+
+</body>
+</html>
