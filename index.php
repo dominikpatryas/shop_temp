c6ab99b4bf064e1ee40150aa09ae8ffc4ace7ee9
Dominik Patryas <dominik.patryas@gmail.com>
2019-06-17 23:09:49 +0200

    sidebar + css

--- index.php
+++ index.php
@@ -1,9 +1,16 @@
 <?php 
+require 'config/config.php';
 include("includes/header.php");
 include("includes/handlers/logout-redirect.php");
+include("includes/sidebar.php");
  ?>
 
+<div class="main-content">
+<?php echo $userLoggedIn; ?>
+</div>
 
+
+</div>
 
 </body>
 </html>
