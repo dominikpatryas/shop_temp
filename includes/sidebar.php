3e22ec0695d8aea71bbd62a5bf8e3263e0dcd857
Dominik Patryas <dominik.patryas@gmail.com>
2019-06-18 15:50:55 +0200

    display item with photo index

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -1,15 +1,16 @@
-<?php
 
-?>
 <div class="page">
     <div id="sidebar">
      <div class="categories">
             <h2>Wybierz kategorie:</h2>
              <ul>
-            <a href="index.php"> <li>Koszulki z krótkim rękawem</li></a> <hr>
+            <a href="index.php?category=koszulka"> <li>Koszulki z krótkim rękawem</li></a> <hr>
             <a href="spodnie"> <li>Koszulki z długim rękawem</li></a> <hr>
             <a href=""> <li>Bluzy</li></a> <hr>
             <a href=""> <li>Buty</li></a> <hr>
         </ul>
      </div>
 </div>
+
+
+
--- index.php
+++ index.php
@@ -5,12 +5,37 @@ include("includes/handlers/logout-redirect.php");
 include("includes/sidebar.php");
  ?>
 
-<div class="main-content">
-<?php echo $userLoggedIn; ?>
-</div>
 
+<div class='main-content'>
+
+<?php 
+   if (isset($_GET['category'])) {
+       $category = $_GET['category'];
+       $query = mysqli_query($con, "SELECT * FROM items WHERE category='$category'");
+       $count = mysqli_num_rows($query);
+
+      
+   }
+
+   while ($row = mysqli_fetch_array($query)) {
+    
+    echo "  
+    <div class='item'>
+        <div class='img-box'>
+            <img src=' ".$row['photo_url'] ." '>
+        </div>
+        <div class='description-box'>
+            <p>Lorem ipsum Lorem ipsum Lorem ipsum. Lorem ipsum
+                Lorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsum.
+            </p>
+        </div>
+    </div> ";
+}
+?>
+</div>
 
 </div>
 
 </body>
+
 </html>
